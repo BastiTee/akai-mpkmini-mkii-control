e20588ffb6aedefc4d045915df816e8c222e19d8
Basti Tee <basti.tee@posteo.de>
2021-04-12 20:49:10 +0200

    Finish basic click commands

--- akai_mpkmini_mkii_ctrl/__main__.py
+++ akai_mpkmini_mkii_ctrl/__main__.py
@@ -7,11 +7,15 @@ import click
 from akai_mpkmini_mkii_ctrl import controller as ctrl
 
 
-@click.group()
+@click.group(help=__doc__)
 @click.option(
     '--preset', '-p', required=True, metavar='NUM',
     type=click.Choice(['0', '1', '2', '3', '4']), default='0',
     help='Preset selector (0 = RAM, 1-4 Stored preset)'
+)
+@click.option(
+    '--midi-port', '-m', required=True, metavar='NUM',
+    type=click.INT, default=0, help='MIDI port'
 )
 @click.option(
     '--verbose', '-v', is_flag=True,
@@ -21,19 +25,53 @@ from akai_mpkmini_mkii_ctrl import controller as ctrl
 def main(
     ctx: click.Context,
     preset: str,
+    midi_port: str,
     verbose: bool
 ) -> None:
     ctx.ensure_object(dict)
     ctx.obj['preset'] = int(preset)
+    ctx.obj['midi_port'] = int(midi_port)
     ctx.obj['verbose'] = verbose
 
 
-@main.command()
+@main.command(help='Print preset in human readable format')
 @click.pass_context
-def print_config(ctx: click.Context) -> None:
-    with ctrl.midi_connection() as (m_in, m_out):
+def print_preset(ctx: click.Context) -> None:
+    with ctrl.midi_connection(ctx.obj['midi_port']) as (m_in, m_out):
         config = ctrl.get_config_from_device(ctx.obj['preset'], m_in, m_out)
         print(config)
+
+
+@main.command(help='Push a binary preset from file to the device')
+@click.option(
+    '--input-file', '-i', required=True, metavar='FILE',
+    help='Binary input file, i.e., a regular *.mk2 preset file'
+)
+@click.pass_context
+def push_preset(
+    ctx: click.Context,
+    input_file: str
+) -> None:
+    with ctrl.midi_connection(ctx.obj['midi_port']) as (m_in, m_out):
+        ctrl.send_binary_to_device(
+            input_file, ctx.obj['preset'], m_out
+        )
+
+
+@main.command(help='Pull a binary from the device and write to file')
+@click.option(
+    '--output-file', '-o', required=True, metavar='FILE',
+    help='Binary output file, i.e., a regular *.mk2 preset file'
+)
+@click.pass_context
+def pull_preset(
+    ctx: click.Context,
+    output_file: str
+) -> None:
+    with ctrl.midi_connection(ctx.obj['midi_port']) as (m_in, m_out):
+        binary = ctrl.get_binary_from_device(ctx.obj['preset'], m_in, m_out)
+        with open(output_file, 'wb') as output_file_handle:
+            output_file_handle.write(bytes(binary))
 
 
 if __name__ == '__main__':
--- akai_mpkmini_mkii_ctrl/controller.py
+++ akai_mpkmini_mkii_ctrl/controller.py
@@ -5,11 +5,11 @@
 import binascii
 from contextlib import contextmanager
 from time import sleep
-from typing import Generator, List, Sequence, Tuple
+from typing import Generator, List, Tuple
 
 import click
 import rtmidi
-from rtmidi import MidiIn, MidiOut, midiutil
+from rtmidi import MidiIn, MidiOut, RtMidiError, midiutil
 
 from akai_mpkmini_mkii_ctrl.mpkmini_mk2 import MPK_MINI_MK2
 
@@ -18,8 +18,9 @@ DEVICE_NAME = 'MPK Mini Mk II'
 
 @contextmanager
 def midi_connection(
+    midi_port: int
 ) -> Generator[Tuple[MidiIn, MidiOut], None, None]:
-    midi_in, midi_out = setup_midi_in_and_out()
+    midi_in, midi_out = setup_midi_in_and_out(midi_port)
     try:
         yield (midi_in, midi_out)
     finally:
@@ -27,9 +28,90 @@ def midi_connection(
         midi_out.close_port()
 
 
-def wait_for_sysex_response(
+def setup_midi_in_and_out(midi_port: int) -> Tuple[MidiIn, MidiOut]:
+    # Setup MIDI sender
+    midi_out = rtmidi.MidiOut()
+    try:
+        midi_out.open_port(midi_port)
+    except RtMidiError as err:
+        __error('Cannot connect to MIDI device.', err)
+    midi_out_port_name = midi_out.get_port_name(midi_port)
+
+    # Setup MIDI receiver
+    midi_in, midi_in_port_name = midiutil.open_midiinput(midi_port)
+    midi_in.ignore_types(sysex=False)  # !! Otherwise no sysex receiver
+
+    # Check that we're actually connected with an AKAI MPKmini MK2
+    if midi_in_port_name != DEVICE_NAME:
+        __error(f'Input device not of type "{DEVICE_NAME}" '
+                + f'but "{midi_in_port_name}"')
+    if midi_out_port_name != DEVICE_NAME:
+        __error(f'Output device not of type "{DEVICE_NAME}" '
+                + f'but "{midi_out_port_name}"')
+
+    return midi_in, midi_out
+
+
+def send_binary_to_device(
+    file_path: str,
+    preset: int,
+    midi_out: MidiOut
+) -> None:
+    # TODO Input validation
+    with open(file_path, 'rb') as in_file_byte:
+        data = in_file_byte.read(2000)
+    config = MPK_MINI_MK2.parse(data)
+    send_config_to_device(config, preset, midi_out)
+
+
+def send_config_to_device(
+    config: MPK_MINI_MK2,
+    preset: int,
+    midi_out: MidiOut
+) -> None:
+    # TODO Input validation
+    config[0].preset = preset
+    data = MPK_MINI_MK2.build(config)
+    assert data[0] == 0xF0 and data[-1] == 0xF7
+    midi_out.send_message(data)
+    data_hex = str(binascii.hexlify(bytearray(data)), 'utf-8')
+    __print(f'- SENT {len(data)} BYTES. SYSEX:\n{data_hex}')
+
+
+def get_binary_from_device(
+    preset: int,
+    midi_in: MidiIn,
+    midi_out: MidiOut
+) -> List[int]:
+    # TODO Input validation
+    send_sysex_from_hex_string(f'f0 47 00 26 66 00 01 0{preset} f7', midi_out)
+    return receive_sysex(midi_in)
+
+
+def get_config_from_device(
+    preset: int,
+    midi_in: MidiIn,
+    midi_out: MidiOut
+) -> MPK_MINI_MK2:
+    # TODO Input validation
+    message = get_binary_from_device(preset, midi_in, midi_out)
+    return MPK_MINI_MK2.parse(bytearray(message))
+
+
+def send_sysex_from_hex_string(
+    hex_string: str,
+    midi_out: MidiOut
+) -> None:
+    data = bytearray.fromhex(hex_string)
+    assert data[0] == 0xF0 and data[-1] == 0xF7
+    midi_out.send_message(data)
+    data_hex = str(binascii.hexlify(bytearray(data)), 'utf-8')
+    __print(f'- SENT {len(data)} BYTES. SYSEX:\n{data_hex}')
+
+
+def receive_sysex(
     midi_in: MidiIn
-) -> Tuple[List[int], Sequence]:
+) -> List[int]:
     message = None
     while True:
         raw_tuple = midi_in.get_message()
@@ -44,75 +126,18 @@ def wait_for_sysex_response(
     message[4] = 100
     message_hex = ''.join([str(hex(m))[2:].zfill(2) for m in message])
     __print(f'- RECEIVED {len(message)} BYTES. SYSEX:\n{message_hex}')
-    return message, MPK_MINI_MK2.parse(bytearray(message))
-
-
-def send_sysex_from_binary_file(
-    file_path: str,
-    midi_out: MidiOut
-) -> None:
-    with open(file_path, 'rb') as in_file_byte:
-        data = in_file_byte.read(2000)
-    data_hex = str(binascii.hexlify(bytearray(data)), 'utf-8')
-    assert data[0] == 0xF0 and data[-1] == 0xF7
-    midi_out.send_message(data)
-    data_hex = str(binascii.hexlify(bytearray(data)), 'utf-8')
-    __print(f'- SENT {len(data)} BYTES. SYSEX:\n{data_hex}')
-
-
-def send_sysex_from_hex_string(
-    hex_string: str,
-    midi_out: MidiOut
-) -> None:
-    data = bytearray.fromhex(hex_string)
-    assert data[0] == 0xF0 and data[-1] == 0xF7
-    midi_out.send_message(data)
-    data_hex = str(binascii.hexlify(bytearray(data)), 'utf-8')
-    __print(f'- SENT {len(data)} BYTES. SYSEX:\n{data_hex}')
-
-
-def setup_midi_in_and_out() -> Tuple[MidiIn, MidiOut]:
-
-    # Setup output
-    midi_out = rtmidi.MidiOut()
-    midi_out.open_port(0)
-
-    # Setup MIDI receiver
-    midi_in, midi_in_port_name = midiutil.open_midiinput(0)
-    midi_in.ignore_types(sysex=False)  # !! Otherwise no sysex receiver
-
-    # Check that we're actually connected with an AKAI MPKmini MK2
-    assert midi_in_port_name == DEVICE_NAME
-    assert midi_out.get_port_name(0) == DEVICE_NAME
-
-    return midi_in, midi_out
-
-
-def get_config_from_device(
-    preset: int,  # 0 = RAM, 1-4 Stored Presets
-    midi_in: MidiIn,
-    midi_out: MidiOut
-) -> MPK_MINI_MK2:
-    assert preset >= 0 and preset <= 4
-    send_sysex_from_hex_string(f'f0 47 00 26 66 00 01 0{preset} f7', midi_out)
-    _, config = wait_for_sysex_response(midi_in)
-    return config
-
-
-def send_config_to_device(
-    config: MPK_MINI_MK2,
-    preset: int,  # 0 = RAM, 1-4 Stored Presets
-    midi_out: MidiOut
-) -> None:
-    assert preset >= 0 and preset <= 4
-    config[0].preset = preset
-    data = MPK_MINI_MK2.build(config)
-    assert data[0] == 0xF0 and data[-1] == 0xF7
-    midi_out.send_message(data)
-    data_hex = str(binascii.hexlify(bytearray(data)), 'utf-8')
-    __print(f'- SENT {len(data)} BYTES. SYSEX:\n{data_hex}')
+    return message
 
 
 def __print(message: str) -> None:
     if click.get_current_context().obj['verbose']:
         print(message)
+
+
+def __error(message: str, err: Exception = None) -> None:
+    if err:
+        print(f'!!! {message}: {err}\n')
+    else:
+        print(f'!!! {message}\n')
+    print(click.get_current_context().get_help())
+    exit(1)
