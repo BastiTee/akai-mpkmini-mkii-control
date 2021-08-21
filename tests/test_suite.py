d0fdfffed34951a89f6c01f04b4b26678e2f499a
Basti Tee <basti.tee@posteo.de>
2021-08-21 09:07:19 +0200

    Remove useless test imports

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
-"""Basic test suite.
-
-There are some 'noqa: F401' in this file to just test the isort import sorting
-along with the code formatter.
-"""
+"""Basic test suite."""
 
 import __future__  # noqa: F401
 
-import json  # noqa: F401
-from os import path  # noqa: F401
-from re import IGNORECASE, sub  # noqa: F401
-
 import pytest
-import requests  # noqa: F401
-
-import akai_mpkmini_mkii_ctrl  # noqa: F401
 
 
 class TestUtils:  # noqa: D101
